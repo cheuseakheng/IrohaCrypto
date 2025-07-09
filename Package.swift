163043827d279b03885d49064bda3adce51594ea
Seakheng Cheu <81610713+SeakhengCheu@users.noreply.github.com>
2025-07-09 11:03:12 +0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "IrohaCrypto",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "IrohaCrypto", targets: ["IrohaCrypto"])
+    ],
+    targets: [
+        .target(
+            name: "IrohaCrypto",
+            path: "IrohaCrypto",
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("include")
+            ]
+        )
+    ]
+)
