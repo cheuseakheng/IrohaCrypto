54b2285fde9aaac3528e51185fc3b6c5358ab1f2
Seakheng Cheu <81610713+cheuseakheng@users.noreply.github.com>
2025-07-14 09:26:08 +0700

    Create cpusupport-ARM-AES.c

---
+++ ScryptHelper/libcperciva/cpusupport/Build/cpusupport-ARM-AES.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+
+#ifdef __ARM_NEON
+#include <arm_neon.h>
+#endif
+
+int
+main(void)
+{
+	uint8x16_t data;
+	uint8x16_t key = {0};
+	uint8x16_t output;
+	uint32x4_t lanes = {0};
+	uint8_t arr[16] = {0};
+
+	/* Check AES. */
+	data = vld1q_u8(arr);
+	output = vaeseq_u8(data, key);
+	(void)output; /* UNUSED */
+
+	/* Check _u32: some compilers only support the _u8 variant. */
+	lanes = vdupq_laneq_u32(lanes, 0);
+
+	/* Success! */
+	return (0);
+}
