2a28017be2068a5c60bdb645763405d2d4e2decd
Seakheng Cheu <81610713+cheuseakheng@users.noreply.github.com>
2025-07-14 09:26:40 +0700

    Add files via upload

---
+++ ScryptHelper/libcperciva/cpusupport/Build/cpusupport-X86-AESNI.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+
+#include <wmmintrin.h>
+
+/*
+ * Use a separate function for this, because that means that the alignment of
+ * the _mm_loadu_si128() will move to function level, which may require
+ * -Wno-cast-align.
+ */
+static __m128i
+load_128(const uint8_t * src)
+{
+	__m128i x;
+
+	x = _mm_loadu_si128((const __m128i *)src);
+	return (x);
+}
+
+int
+main(void)
+{
+	__m128i x, y;
+	uint8_t a[16] = {0};
+
+	x = load_128(a);
+#ifdef BROKEN_MM_LOADU_SI64
+	y = _mm_loadu_si128((const __m128i *)a);
+#else
+	y = _mm_loadu_si64(a);
+#endif
+	y = _mm_aesenc_si128(x, y);
+	_mm_storeu_si128((__m128i *)&a[0], y);
+	return (a[0]);
+}
